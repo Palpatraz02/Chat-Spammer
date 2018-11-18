fc7880d109fa7461d3186fbbea1fe3c01a333243
PierreV2002 <vicquery.pierre@gmail.com>
2018-11-18 20:26:45 +0100

    Neon BETA

--- Chat Spammer C#/FluentSystem/MainWindow.xaml.cs
+++ Chat Spammer C#/FluentSystem/MainWindow.xaml.cs
@@ -20,21 +20,80 @@ namespace FluentSystem
 
     public partial class MainWindow
     {
-        string testo;
+        
         public MainWindow()
         {
             InitializeComponent();
         }
         
-        private void Button1_Click(object sender, RoutedEventArgs e)
+
+        private void intervallo_TextChanged(object sender, TextChangedEventArgs e) {
+
+            string temp = "";
+            for (int i = 0; i < intervallo.Text.Length; i++)
+            {
+                char a = intervallo.Text[i];
+                if (a == '0' || a == '1' || a == '2' || a == '3' || a == '4' || a == '5' || a == '6' || a == '7' || a == '8' || a == '9')
+                {
+                    temp += intervallo.Text[i];
+                }
+            }
+            intervallo.Text = temp;
+
+        }
+        public bool finish = false;
+        private void Button1_Click_1(object sender, RoutedEventArgs e)
         {
-            SendKeys.Send("C");
+
+            Window2 new2 = new Window2();
+            
+           
+
+            System.Windows.Forms.MessageBox.Show("Inizio countdown!");
+            System.Threading.Thread.Sleep(5000);
+            new2.Show();
+            for (int i = 0; i < Int32.Parse(numero.Text)||finish!=false; i++)
+            {
+                System.Threading.Thread.Sleep(Int32.Parse(intervallo.Text));
+                SendKeys.SendWait(messaggio.Text);
+      
+                SendKeys.SendWait("{ENTER}");
+            }
+            finish = false;
+        }
+
+        private void Button2_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window1 newwindow = new Window1();
+            newwindow.Show();
+        }
+
+        private void numero_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        
+            string temp = "";
+            for (int i = 0; i < numero.Text.Length; i++)
+            {
+                char a = numero.Text[i];
+                if (a == '0' || a == '1' || a == '2' || a == '3' || a == '4' || a == '5' || a == '6' || a == '7' || a == '8' || a == '9')
+                {
+                    temp += numero.Text[i];
+                }
+            }
+            numero.Text = temp;
+          
+      
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+           
+
         }
+
+        
     }
-    }
+}
 
